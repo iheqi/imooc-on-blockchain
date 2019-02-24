93e2665326c5e4dda202c40537d4221293220d0d
rubbish-man <993284758@qq.com>
2019-02-24 20:37:56 +0800

    课程列表合约实现

--- contracts/Imooc.sol
+++ contracts/Imooc.sol
@@ -2,8 +2,24 @@ pragma solidity ^0.5.0;
 
 contract CourseList {
     address public ceo;
-
+    address[] public courses;
     constructor() public {
         ceo = msg.sender;
+    }
+
+    function createCourse(string memory _name) public {
+        address newCourse = address(new Course(_name));
+        courses.push(newCourse);
+    }
+
+    function getCourse() public view returns (address[] memory) {
+        return courses;
+    }
+}
+
+contract Course {
+    string public name;
+    constructor(string memory _name) public {
+        name = _name;
     }
 }

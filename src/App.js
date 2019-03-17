51bca786941d0748bda49e635e5e7d0366ce6926
rubbish-man <993284758@qq.com>
2019-03-17 14:48:55 +0800

    头部导航组件开发

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import { Button } from 'antd';
 import { BrowserRouter, Route, Link } from "react-router-dom";
+import { Layout } from 'antd';
+import Header from './components/Header';
 
+const { Content, Footer } = Layout;
 const Course = () => <span>课程</span>
 const Qa = () => <span>问答区</span>
 const Create = () => <span>我要众筹</span>
@@ -10,7 +12,12 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter className="App">
-        <div>
+        <Layout>
+          <Header></Header>
+          <Content>Content</Content>
+          <Footer>Footer</Footer>
+        </Layout>
+        {/* <div>
           <ul>
             <li>
               <Link to="/">课程</Link>
@@ -26,7 +33,7 @@ class App extends Component {
           <Route path="/" exact component={Course}></Route>
           <Route path="/qa" exact component={Qa}></Route>
           <Route path="/create" exact component={Create}></Route>
-        </div>
+        </div> */}
       </BrowserRouter>
     );
   }
---
+++ src/components/Header.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Layout, Menu } from 'antd';
+import { Link, withRouter } from 'react-router-dom';
+import MenuItem from 'antd/lib/menu/MenuItem';
+
+const Header = Layout.Header;
+
+class HeadComp extends React.Component {
+  render() {
+    return (
+      <Header>
+        <div className="logo">
+          <img src="/logo.png" alt="logo"></img>
+        </div>
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          style={{lineHeight: '64px'}}
+          defaultSelectedKeys={[this.props.location.pathname]}
+        >
+          <MenuItem key="/">
+            <Link to="/">首页</Link>
+          </MenuItem>
+          <MenuItem key="/qa">
+            <Link to="/qa">问答区</Link>
+          </MenuItem>
+          <MenuItem key="/create">
+            <Link to="/create">我要众筹</Link>
+          </MenuItem>                    
+        </Menu>
+      </Header>
+    );
+  }
+}
+
+export default withRouter(HeadComp);

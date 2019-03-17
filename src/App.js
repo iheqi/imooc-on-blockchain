948b70fc055e3e95cf50f31f02c461a50068b80a
rubbish-man <993284758@qq.com>
2019-03-17 13:56:00 +0800

    引入react-router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
 import { Button } from 'antd';
-import './App.css';
+import { BrowserRouter, Route, Link } from "react-router-dom";
+
+const Course = () => <span>课程</span>
+const Qa = () => <span>问答区</span>
+const Create = () => <span>我要众筹</span>
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Button>测试</Button>
-      </div>
+      <BrowserRouter className="App">
+        <div>
+          <ul>
+            <li>
+              <Link to="/">课程</Link>
+            </li>
+            <li>
+              <Link to="/qa">问答区</Link>
+            </li>
+            <li>
+              <Link to="/create">新建</Link>
+            </li>
+          </ul>
+
+          <Route path="/" exact component={Course}></Route>
+          <Route path="/qa" exact component={Qa}></Route>
+          <Route path="/create" exact component={Create}></Route>
+        </div>
+      </BrowserRouter>
     );
   }
 }

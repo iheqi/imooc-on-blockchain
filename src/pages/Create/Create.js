8e298ace97bd6d5bef10f9f34c0cdb9eabf7d6f6
rubbish-man <993284758@qq.com>
2019-04-12 21:52:23 +0800

    创建课程表单验证

--- src/pages/Create/Create.js
+++ src/pages/Create/Create.js
@@ -22,53 +22,61 @@ class Create extends React.Component {
   handleSubmit = async (e) => {
     console.log(this.state);
     e.preventDefault();
-
-    const accounts = await web3.eth.getAccounts();
-    const arr = [
-      this.state.name,
-      this.state.content,
-      web3.utils.toWei(this.state.price),
-      web3.utils.toWei(this.state.fundingPrice),
-      web3.utils.toWei(this.state.target),
-      this.state.img
-    ];
-    console.log(arr, courseList.methods.createCourse);
-
-    await courseList.methods.createCourse(...arr).send({
-      from: accounts[0],
-      gas: 5000000
-    }, (error, hash) => {
-      console.log('创建课程成功');
-      this.setState({
-        toHomePage: true
+    this.props.form.validateFieldsAndScroll(async (err, values) => { // 验证是异步的
+      if (err) {
+        return;
+      } 
+      const accounts = await web3.eth.getAccounts();
+      const arr = [
+        this.state.name,
+        this.state.content,
+        web3.utils.toWei(this.state.price),
+        web3.utils.toWei(this.state.fundingPrice),
+        web3.utils.toWei(this.state.target),
+        this.state.img
+      ];
+      console.log(arr, courseList.methods.createCourse);
+  
+      await courseList.methods.createCourse(...arr).send({
+        from: accounts[0],
+        gas: 5000000
+      }, (error, hash) => {
+        console.log('创建课程成功');
+        this.setState({
+          toHomePage: true
+        });
       });
     });
-
   }
 
   handleOnlineSubmit = async (e) => {
     console.log(this.state);
     e.preventDefault();
+    this.props.form.validateFieldsAndScroll(async (err, values) => {
+      console.log(!!err);
 
-    const accounts = await web3.eth.getAccounts();
-    const arr = [
-      this.state.name,
-      this.state.content,
-      web3.utils.toWei(this.state.price),
-      this.state.img
-    ];
-    console.log(arr);
-
-    await courseList.methods.createOnlineCourse(...arr).send({
-      from: accounts[0],
-      gas: 5000000
-    }, (error, hash) => {
-      console.log('创建课程成功');
-      this.setState({
-        toHomePage: true
-      });
+      if (err) {
+        return;
+      }
+      const accounts = await web3.eth.getAccounts();
+      const arr = [
+        this.state.name,
+        this.state.content,
+        web3.utils.toWei(this.state.price),
+        this.state.img
+      ];
+      console.log(arr);
+  
+      await courseList.methods.createOnlineCourse(...arr).send({
+        from: accounts[0],
+        gas: 5000000
+      }, () => {
+        console.log('创建课程成功');
+        this.setState({
+          toHomePage: true
+        });
+      });      
     });
-
   }
 
   handleUpload = async (file) => {
@@ -103,6 +111,7 @@ class Create extends React.Component {
     if (this.state.toHomePage) {
       return <Redirect to="/"></Redirect>
     }
+    const { getFieldDecorator } = this.props.form;
     return (
       <Tabs defaultActiveKey="1" onChange={this.handleTabChange}>
         <TabPane tab="直接创建" key="1">
@@ -115,23 +124,51 @@ class Create extends React.Component {
             <Col span={20}>
               <Form onSubmit={this.handleOnlineSubmit}>
                 <FormItem label="课程名">
-                  <Input name="name" onChange={this.onChange} value={this.state.name}></Input>
+                  {getFieldDecorator('name', {
+                    rules: [
+                      { required: true, message: '请输入课程名!' },
+                    ],
+                    initialValue: this.state.name
+                  })(
+                    <Input name="name" onChange={this.onChange}></Input>
+                  )}
                 </FormItem>
                 <FormItem label="课程详情">
-                  <Input.TextArea row={6} name="content" onChange={this.onChange} value={this.state.content}></Input.TextArea>
+                  {getFieldDecorator('content', {
+                    rules: [
+                      { required: true, message: '请输入课程详情!' },
+                    ],
+                    initialValue: this.state.content
+                  })(
+                    <Input.TextArea row={6} name="content" onChange={this.onChange}></Input.TextArea>
+                  )}                
                 </FormItem>  
                 <FormItem label="课程封面">
-                  <Upload beforeUpload={this.handleUpload} showUploadList={false}>
-                    {
-                      this.state.img ? 
-                        <img height="100px" src={`${ipfsPrefix}${this.state.img}`} alt="课程封面"/>
-                          :
-                        <Button>上传图片</Button>
-                    }
-                  </Upload>
+                  {getFieldDecorator('img', {
+                    rules: [
+                      { required: true, message: '请输入选择一张图片作为封面!' },
+                    ],
+                  })(
+                    <Upload beforeUpload={this.handleUpload} showUploadList={false}>
+                      {
+                        this.state.img ? 
+                          <img height="100px" src={`${ipfsPrefix}${this.state.img}`} alt="课程封面"/>
+                            :
+                          <Button>上传图片</Button>
+                      }
+                    </Upload>
+                  )}                 
+
                 </FormItem>  
                 <FormItem label="上线价格">
-                  <Input name="price" onChange={this.onChange} value={this.state.price}></Input>
+                  {getFieldDecorator('price', {
+                    rules: [
+                      { required: true, message: '请输入课程上线价格!' },
+                    ],
+                    initialValue: this.state.price
+                  })(
+                    <Input name="price" onChange={this.onChange}></Input>
+                  )}                  
                 </FormItem>
                 <FormItem>
                   <Button htmlType="submit">添加课程</Button>
@@ -150,29 +187,57 @@ class Create extends React.Component {
             <Col span={20}>
               <Form onSubmit={this.handleSubmit}>
                 <FormItem label="课程名">
-                  <Input name="name" onChange={this.onChange} value={this.state.name}></Input>
+                {getFieldDecorator('name', {
+                  rules: [
+                    { required: true, message: '请输入课程名!' },
+                  ],
+                  initialValue: this.state.name
+                })(
+                  <Input name="name" onChange={this.onChange}></Input>
+                )}
                 </FormItem>
                 <FormItem label="课程详情">
-                  <Input.TextArea row={6} name="content" onChange={this.onChange} value={this.state.content}></Input.TextArea>
+                  {getFieldDecorator('content', {
+                    rules: [
+                      { required: true, message: '请输入课程详情!' },
+                    ],
+                    initialValue: this.state.content
+                  })(
+                    <Input.TextArea row={6} name="content" onChange={this.onChange}></Input.TextArea>
+                  )}                
                 </FormItem>  
                 <FormItem label="课程封面">
-                  <Upload beforeUpload={this.handleUpload} showUploadList={false}>
-                    {
-                      this.state.img ? 
-                        <img height="100px" src={`${ipfsPrefix}${this.state.img}`} alt="课程封面"/>
-                          :
-                        <Button>上传图片</Button>
-                    }
-                  </Upload>
+                  {getFieldDecorator('img', {
+                    rules: [
+                      { required: true, message: '请输入选择一张图片作为封面!' },
+                    ],
+                  })(
+                    <Upload beforeUpload={this.handleUpload} showUploadList={false}>
+                      {
+                        this.state.img ? 
+                          <img height="100px" src={`${ipfsPrefix}${this.state.img}`} alt="课程封面"/>
+                            :
+                          <Button>上传图片</Button>
+                      }
+                    </Upload>
+                  )}                 
+
                 </FormItem>  
                 <FormItem label="众筹目标">
-                  <Input name="target" onChange={this.onChange} value={this.state.target}></Input>
+                  <Input name="target" onChange={this.onChange}></Input>
                 </FormItem>          
                 <FormItem label="众筹价格">
-                  <Input name="fundingPrice" onChange={this.onChange} value={this.state.fundingPrice}></Input>
+                  <Input name="fundingPrice" onChange={this.onChange}></Input>
                 </FormItem>
                 <FormItem label="上线价格">
-                  <Input name="price" onChange={this.onChange} value={this.state.price}></Input>
+                  {getFieldDecorator('price', {
+                    rules: [
+                      { required: true, message: '请输入课程上线价格!' },
+                    ],
+                    initialValue: this.state.price
+                  })(
+                    <Input name="price" onChange={this.onChange}></Input>
+                  )}                  
                 </FormItem>
                 <FormItem>
                   <Button htmlType="submit">添加课程</Button>
@@ -187,4 +252,6 @@ class Create extends React.Component {
   }
 }
 
-export default Create;
+const WrappedDynamicRule = Form.create({ name: 'form' })(Create);
+
+export default WrappedDynamicRule;
